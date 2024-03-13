c9de61036c9c84095989b39bbed4d939e8a897b3
yusufftt <syaifuly482@gmail.com>
2024-03-13 20:26:48 +1400

    web ccontrol

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/Login', [LoginController::class, 'index']);
+
+Route::get('/Register', [RegisterController::class, 'index']);
